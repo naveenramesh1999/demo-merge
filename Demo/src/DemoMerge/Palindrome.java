b832f77f75135c6ba0a49958f51e0d6f539fec95
Naveenkumar.R <Naveenkumar.R@ATMECSINLT-1203.ATMECS.CORP>
2022-08-04 23:24:42 +0530

    first commit

---
+++ Demo/src/DemoMerge/Palindrome.java
@@ -0,0 +1,24 @@
+package DemoMerge;
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+	public void checkPalindrome() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the string");
+		String string = scanner.next();
+		String reverse = "";
+		scanner.close();
+		for (int value = string.length() - 1; value >= 0; value--) {
+
+			reverse = reverse + string.charAt(value);
+		}
+		if (string.equals(reverse)) {
+			System.out.println("The given string is palindrome");
+		} else {
+			System.out.println("The given string not a palindrome");
+		}
+	}
+
+}
